5f2df028e8df58878b766049a58d3bbb547c16bd
EvgeniyaPas <evgeniya.alem@gmail.com>
2024-10-06 21:24:06 +0300

    Add files via upload

---
+++ module_3.1.py
@@ -0,0 +1,20 @@
+calls = 0
+def count_calls():
+    global calls
+    calls = calls + 1
+def string_info(string):
+    tuple = (len(string), string.lower(), string.upper())
+    print(tuple)
+    count_calls()
+    return
+def is_contains(string,list_to_search):
+    print(string in list_to_search)
+    count_calls()
+string_info('Холодильник')
+string_info('Стол')
+is_contains('BANan', ['ban', 'BaNaN', 'urBAN'])
+is_contains('cyclic', ['recycling', 'cyclic'])
+print(calls)
+
+
+
