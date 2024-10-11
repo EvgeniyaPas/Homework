294a2d4d6a122ccc0f0a3909d1be0999921bd94f
EvgeniyaPas <evgeniya.alem@gmail.com>
2024-10-11 17:50:23 +0300

    Add files via upload

--- module_3.1.py
+++ module_3.1.py
@@ -7,12 +7,15 @@ def string_info(string):
     print(tuple)
     count_calls()
 def is_contains(string,list_to_search):
+    for i in range(len(list_to_search)):
+        list_to_search[i] = list_to_search[i].lower()
+    string = string.lower()
     print(string in list_to_search)
     count_calls()
 string_info('Холодильник')
 string_info('Стол')
 is_contains('BANan', ['ban', 'BaNaN', 'urBAN'])
-is_contains('cyclic', ['recycling', 'cyclic'])
+is_contains('Cyclic', ['recycling', 'cyclic'])
 print(calls)
 
 
