7ebd3c0c38b121f73af70436313cebb30356f109
EvgeniyaPas <evgeniya.alem@gmail.com>
2024-11-16 08:44:47 +0300

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -1,8 +1,5 @@
 from pprint import pprint
 
-from unicodedata import category
-
-
 class Product:
     def __init__(self, name, weight, category):
         self.name = name
@@ -12,27 +9,27 @@ class Product:
     def __str__(self):
         return f'{self.name}, {self.weight}, {self.category}'
 
-class Shop(Product):
+class Shop():
     __file_name = 'products.txt'
 
     def get_products(self):
-        file = open ('products.txt', 'r')
-        pprint(file.read())
-        file.close()
+        with open('products.txt') as file:
+            s = file.read()
+            file.close()
+        return s
 
     def add(self, *products):
         self.products = products
-        print(*products)
-        # super().__init__()
-        file = open('products.txt', 'r')
-        for self.name in  file:
-            print(f'Продукт {self.products} уже есть в магазине')
-            file.close()
+        spisok_productov = self.get_products()
 
-        else:
-            file = open('products.txt', 'a')
-            file.write(f'{self.name}, {self.weight}, {self.category}')
-            file.close()
+        for i in products:
+            if str(i) not in spisok_productov:
+                file = open('products.txt', 'a')
+                file.write(f'{i}\n')
+                file.close()
+
+            else:
+                print(f'Продукт {i} уже есть в магазине')
 
 
 s1 = Shop()
@@ -41,9 +38,5 @@ p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
 print(p2) # __str__
-print(s1.get_products())
-
 s1.add(p1, p2, p3)
-
 print(s1.get_products())
-
